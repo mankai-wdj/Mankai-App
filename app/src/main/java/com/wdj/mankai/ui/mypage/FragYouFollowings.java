f49cc8c5305f6906ac26e634647c8801038d73d6
suleun <teaaa030@gmail.com>
2022-05-16 09:25:35 +0900

    you followings

--- app/src/main/java/com/wdj/mankai/ui/mypage/FragYouFollowings.java
+++ app/src/main/java/com/wdj/mankai/ui/mypage/FragYouFollowings.java
@@ -1,66 +1,118 @@
 package com.wdj.mankai.ui.mypage;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.wdj.mankai.R;
+import com.wdj.mankai.data.FollowingData;
+import com.wdj.mankai.data.model.AppHelper;
+import com.wdj.mankai.ui.mypage.RecyclerView.FollowingsAdapter;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragYouFollowings#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class FragYouFollowings extends Fragment {
+    private View view;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private ArrayList<FollowingData> list = new ArrayList<FollowingData>();
+    private FollowingData followingData;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView youFollowings_recyclerview;
+    private FollowingsAdapter followingsAdapter;
+
+    String url;
+    String userId;
 
     public FragYouFollowings() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragYouFollowings.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragYouFollowings newInstance(String param1, String param2) {
-        FragYouFollowings fragment = new FragYouFollowings();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static FragYouFollowings newInstance() {
+        FragYouFollowings fragYouFollowings = new FragYouFollowings();
+        return fragYouFollowings;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frag_you_followings, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.frag_myfollowings,container,false);
+        url = "https://api.mankai.shop/api/follows/";
+
+        youFollowings_recyclerview = view.findViewById(R.id.myFollowings_recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this.getContext(), LinearLayoutManager.VERTICAL, false);
+        youFollowings_recyclerview.setLayoutManager(layoutManager);
+
+        followingsAdapter = new FollowingsAdapter(list);
+        youFollowings_recyclerview.setAdapter(followingsAdapter);
+
+
+
+        followingsAdapter.setOnItemClickListener(new FollowingsAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                list.get(position);
+                Intent intent = new Intent(getActivity(), YouPage.class);
+//                 user id 넣은 url 전송
+                intent.putExtra("youURL", list.get(position).getId());
+                startActivity(intent);
+            }
+        });
+
+        if(AppHelper.requestQueue == null)
+            AppHelper.requestQueue = Volley.newRequestQueue(getContext());
+
+        SharedPreferences sharedPreferences= getActivity().getSharedPreferences("userId", Context.MODE_PRIVATE);
+        userId = sharedPreferences.getString("userId","");
+
+        memoResponse();
+
+        return view;
     }
+    private void memoResponse() {
+        StringRequest myPageStringRequest = new StringRequest(
+                Request.Method.GET, url+userId,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.i("responseInFollowers", response);
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                followingData = new FollowingData(jsonObject.getString("name"), jsonObject.getString("profile"), jsonObject.getString("id"));
+                                list.add(followingData);
+                                Log.d("TEXT", list.get(i).getId());
+                            }
+                            followingsAdapter.notifyDataSetChanged();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },null);
+        myPageStringRequest.setShouldCache(false);
+        AppHelper.requestQueue.add(myPageStringRequest);
+    }
+
 }
